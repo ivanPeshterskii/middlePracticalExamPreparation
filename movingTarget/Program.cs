22de31095183bc8bdb2ae5101b342c07b382de33
ivanPeshterskii <peshterskiivan@gmail.com>
2025-06-16 22:04:42 +0300

    Add files via upload

---
+++ movingTarget/Program.cs
@@ -0,0 +1,71 @@
+﻿List<int> targets = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+string command = string.Empty;
+
+while ((command=Console.ReadLine())!="End")
+{
+    string[] tokens = command.Split();
+
+    string action = tokens[0];
+    int index = int.Parse(tokens[1]);
+
+    switch (action)
+    {
+        case "Shoot":
+            int power = int.Parse(tokens[2]);
+            Shoot(index,power,targets);
+            break;
+
+        case "Add":
+            int value = int.Parse(tokens[2]);
+            Add(index,value,targets);
+            break;
+
+        case "Strike":
+            int radius = int.Parse(tokens[2]);
+            strike(index, radius, targets);
+            break;
+    }
+}
+
+static void Shoot(int index, int power, List<int> targets)
+{
+    if (index<0||index>targets.Count-1)
+    {
+        return;
+    }
+
+    targets[index] -= power;
+
+    if (targets[index]<=0)
+    {
+        targets.RemoveAt(index);
+    }
+}
+
+static void Add(int index, int value, List<int> targets)
+{
+    if (index < 0 || index > targets.Count - 1)
+    {
+        Console.WriteLine("Invalid placement!");
+        return;
+    }
+
+    targets.Insert(index,value);
+}
+
+static void strike(int index, int radius, List<int> targets)
+{
+    if (index - radius < 0 || index + radius > targets.Count - 1)
+    {
+        Console.WriteLine("Strike missed!");
+
+        return;
+    }
+    targets.RemoveRange(index-radius,radius*2+1);
+}
+
+Console.WriteLine(string.Join("|",targets));
---
+++ theLift/Program.cs
@@ -0,0 +1,31 @@
+﻿int peopleWaiting = int.Parse(Console.ReadLine()); 
+
+int[] lift = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int maxPeoplePerWagno = 4;
+
+for (int i = 0; i < lift.Length; i++)
+{
+    int current = lift[i];
+    for (int j = current; j < maxPeoplePerWagno; j++)
+    {
+        lift[i]++;
+        peopleWaiting--;
+
+        if (peopleWaiting==0)
+        {
+            if (lift.Sum() < lift.Length * maxPeoplePerWagno)
+            {
+                Console.WriteLine("The lift has empty spots!");
+            }
+            
+            Console.WriteLine(string.Join(' ', lift));
+            return;
+        }
+    }
+}
+Console.WriteLine($"There isn't enough space! {peopleWaiting} people in a queue!");
+Console.WriteLine(string.Join(' ',lift));
